181af4abc25a6be80b814b7840a2cd312af428f1
Leefrost <evertodante@gmail.com>
2022-12-17 23:00:20 +0200

    Day 1 - dotnet

--- csharp/Runner/Program.cs
+++ csharp/Runner/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+var subset = 0;
+var callories = new List<int>();
+
+using var file = File.OpenRead("Days//input-1.txt");
+using var reader = new StreamReader(file);
+while (!reader.EndOfStream)
+{
+    var line = reader.ReadLine();
+
+    if (line == "")
+    {
+        callories.Add(subset);
+        subset = 0;
+    }
+    else
+    {
+        var current = int.Parse(line);
+        subset += current;
+    }
+}
+
+var max = callories.Max();
+var topMax = callories.OrderByDescending(x=>x)
+    .Take(3).Sum();
+
+Console.WriteLine();
